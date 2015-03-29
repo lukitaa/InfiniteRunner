e50d9e6f17e6731bf24a9692fca47976203ebda1
Luca Giordano <ll.giordano2@gmail.com>
2015-03-29 20:15:31 -0300

    Fixed player horizontal movement
    Fixed the way the players moves horizontally. It has 3 places where the
    player can be placed at only and MovesTowards it when a key is pressed.

--- Assets/Scripts/PlayerManagerScript.cs
+++ Assets/Scripts/PlayerManagerScript.cs
@@ -8,11 +8,18 @@ public class PlayerManagerScript : MonoBehaviour {
 	public Transform playerPrefab;
 	public float distanceToGround;
 	public float movementSize;
+	public float movementSpeed;
+	public bool isMoving {
+		get { 
+			return (moveTo != Vector3.zero) ? true : false;	
+		}
+		set{ _isMoving = value; }
+	}
 	//Private variables
 	Transform playerInstance;
 	float currentHorizontalPosition;
 	Vector3 moveTo;
-	bool isLerping;
+	bool _isMoving;
 
 	// Use this for initialization
 	void Awake () {
@@ -48,7 +55,8 @@ public class PlayerManagerScript : MonoBehaviour {
 			//Move the player
 			Vector3 newPosition = playerInstance.position;
 			newPosition.x += movementSize * horizontalMovement;
-			playerInstance.position = newPosition;
+			//playerInstance.position = newPosition;
+			moveTo = newPosition;
 			//Change the current position
 			currentHorizontalPosition += horizontalMovement;
 		}
@@ -57,6 +65,22 @@ public class PlayerManagerScript : MonoBehaviour {
 	//If the player is hitting the ground returns true, otherwise false
 	bool canJump(){
 		return Physics.Raycast(playerInstance.transform.position, -Vector3.up, distanceToGround);
+	}
+
+	//
+	void Update(){
+		//If there's not a point where the player should move to do nothing
+		if (!isMoving)
+			return;
+		//Move the object based on what type of movement the object has
+		playerInstance.position = Vector3.MoveTowards(playerInstance.position, moveTo, Time.deltaTime * movementSpeed);
+		//Get the distance squared of the current position of the object to the position of the point to go of the path
+		float distanceSquared = (playerInstance.position - moveTo).sqrMagnitude;
+		//Check if the distance position is less than the MaxDistanceToGoal set in beforehand to start
+		//Moving to the next point, in that case, get the next position that we should move to
+		if (distanceSquared < 0.2f * 0.2f)
+			moveTo = Vector3.zero;
+
 	}
 
 }
